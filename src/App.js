d2754a04a159a03cbd1a3394c84556482e618df7
Floandco <renaudflorence02@gmail.com>
2023-01-19 22:36:08 +0100

    My-first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+// import { PromiseProvider } from 'mongoose';
+import logo from '../assets/images/deliveroo-plus.svg';
 import './App.css';
+import axios from "axios";
+
 
 function App() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("https://localhost:3001");
+      setData(response.data);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="bloc-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
       </header>
+  {/* ------------------------------------------------------------ */}
+      <hero>
+        <div>
+          <section className="restaurant">
+            <h1>{data.restaurant.name}</h1>
+            <p>{data.restaurant.description}</p>
+          </section>
+          <section className="img-resto">
+            <img 
+              src={data.restaurant.picture} 
+              className="restaurant-picture" 
+              alt="image d'un plat proposé par le restaurant" />
+          </section>
+        </div>
+      </hero>
+  {/* ------------------------------------------------------------ */}
+      <main>
+        <div className="menu">
+          <div className="menu-categories">
+            <div className="categories">
+              <h2>
+                {data.categories.name}
+              </h2> 
+              categories.meals}
+            
+            </div>
+
+          </div>
+      
+          {/* boucle pour chaque categorie */}
+          {/* demander l'affichage pour chaque categorie : MENU & IMG */}
+
+
+        </div>
+        <div className="panier">
+
+        </div>
+      </main>
+
     </div>
   );
 }
